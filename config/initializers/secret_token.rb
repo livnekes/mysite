3389fd190fe1e57ffcb7573e6210ed473ee3e292
livnekes <livnek@gmail.com>
2015-03-20 14:47:35 -0400

    change app name

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mysite::Application.config.secret_key_base = '6c049cfe8a372e3e2f097f4185774dcce2c005613df3c574aaa37dc270190a67906ccee9c39656704106617fa1c6cbd5e999dc03358c3366f69356d958c73ea9'
